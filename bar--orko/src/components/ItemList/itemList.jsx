769425a04972e8f60441686560cf6c910e5b95a3
Diego Sandoval <diego.sandoval.indiedev@gmail.com>
2024-06-26 16:09:41 -0400

    third

--- bar--orko/src/components/ItemList/itemList.jsx
+++ bar--orko/src/components/ItemList/itemList.jsx
@@ -2,24 +2,23 @@ import React from "react";
 
 const ItemList = ({ productos, agregarAlCarrito }) => {
     return (
-        <div className="row">
-            {productos.map((prod, index) => (
-                <div key={index} className="col-md-4 mb-4">
-                    <div className="card border-5 border-warning h-100">
-                        <img className="card-img-top mx-auto" src={prod.foto_del_producto} alt={prod.trago} />
-                        <div className="card-body">
-                            <h5 className="card-title">{prod.trago}</h5>
-                            <p className="card-text">Precio: ${prod.Precio}</p>
-                            <button className="btn btn-primary" onClick={() => agregarAlCarrito(prod.trago, prod.Precio)}>Agregar al carrito</button>
+        <div className="container">
+            <div className="row justify-content-center">
+                {productos.map((prod, index) => (
+                    <div key={index} className="col-md-4 col-lg-4 mb-4">
+                        <div className="card border-5 border-warning h-100 text-center">
+                            <img className="card-img-top mx-auto" src={prod.foto_del_producto} alt={prod.trago} style={{ width: '120px', height: '400px' }} />
+                            <div className="card-body">
+                                <h5 className="card-title">{prod.trago}</h5>
+                                <p className="card-text">Precio: ${prod.Precio}</p>
+                                <button className="btn btn-primary" onClick={() => agregarAlCarrito(prod.trago, prod.Precio)}>Agregar al carrito</button>
+                            </div>
                         </div>
                     </div>
-                </div>
-            ))}
+                ))}
+            </div>
         </div>
     );
 };
 
 export default ItemList;
-
-
-
